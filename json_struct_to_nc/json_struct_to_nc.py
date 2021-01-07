3652d80ef342170e14d95df2df775f793967e048
Pedro Montero <pmontero@intecmar.gal>
2021-01-07 09:46:02 +0100

    _Fillvalue is read from json field fill_value Cleaning code

--- json_struct_to_nc/json_struct_to_nc.py
+++ json_struct_to_nc/json_struct_to_nc.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-**json_struc_to_nc.py**
+**json_struct_to_nc.py**
 
 * *Purpose:* Read a json file with a struct and return a ncdf file with this structure
 
@@ -11,8 +11,8 @@
 * *license:* INTECMAR
 * *requires:*
 * *date:* 2021/01/04
-* *version:* 0.0.5
-* *date version* 2021/01/04
+* *version:* 0.9.0
+* *date version* 2021/01/07
 
 """
 import sys
@@ -23,7 +23,7 @@ from collections import OrderedDict
 import netCDF4
 
 
-def json_struc_to_nc(input_json):
+def json_struct_to_nc(input_json):
     """
     Read a json file with a structure and save it in a netcdf file
     :param input_json: name of a json file with the options
@@ -48,37 +48,30 @@ def json_struc_to_nc(input_json):
     with open(json_file) as f:
         json_data = json.load(f)
 
+    # Write netCDF
     with netCDF4.Dataset(nc_file, "w", format="NETCDF4") as nc:  # TODO: tipo de netcdf debe de ser leido y no impuesto
+        # Dimensions
         for dimension in json_data['dimensions']:
-
             nc.createDimension(dimension['name'], dimension['size'])
+        # Global attributes
         for gl_att in json_data['global attributes']:
-
             nc.setncattr(gl_att['name'], gl_att['value'])
+        # Variables
         for var in json_data['variables']:
-            print('....', var['name'], var['type'], (* var['dimensions'],))
-            fill_value = None
-            for attr_var in var['attributes']:
-                if attr_var['name'] == '_FillValue':
-                    fill_value = attr_var['value']
-
+            fill_value = var['fill_value']
             nc_var = nc.createVariable(var['name'],
                                        var['type'],
                                        (* var['dimensions'],),
                                        fill_value=fill_value)
-
+            # Attributes of a variable
             for attr_var in var['attributes']:
-                print(attr_var['name'], attr_var['value'])
-                if attr_var['name'] != '_FillValue':
-                    if attr_var['type'] == 'int8':
-                        attr_var['value'] = numpy.int8(attr_var['value'])
-                    nc_var.setncattr(attr_var['name'], attr_var['value'])
-
-
-
-
+                if attr_var['type'] == 'ndarray':
+                    attr_var['value'] = numpy.dtype(attr_var['type_element']).type(attr_var['value'])
+                else:
+                    attr_var['value'] = numpy.dtype(attr_var['type']).type(attr_var['value'])
+                nc_var.setncattr(attr_var['name'], attr_var['value'])
 
 
 if __name__ == '__main__':
     input_json = 'json_struct_to_nc.json'
-    json_struc_to_nc(input_json)
+    json_struct_to_nc(input_json)
