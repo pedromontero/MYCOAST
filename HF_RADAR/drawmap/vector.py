89e5eb3597d5b93214c910b3bc4e2969d715a4ff
Pedro Montero <pmontero@intecmar.gal>
2021-06-20 21:37:30 +0200

    vector.py init from u,v and mod, theta

--- HF_RADAR/drawmap/vector.py
+++ HF_RADAR/drawmap/vector.py
@@ -2,12 +2,19 @@ from math import pi, sqrt, atan2, sin, cos
 
 
 class Vector:
-    def __init__(self, wind=False):
+    def __init__(self, u=None, v=None, mod=None, theta=None, wind=False):
         self.wind = wind
 
-    def set_uv(self, u, v):
         self.u = u
         self.v = v
+        self.mod = mod
+        self.theta = theta
+
+        if self.mod is None or self.theta is None:
+            self.mod, self.theta = self.get_modtheta()
+
+        if self.u is None or self.v is None:
+            self.u, self.v = self.get_uv()
 
     def get_modtheta(self):
         """
@@ -26,3 +33,23 @@ class Vector:
         mod = sqrt(self.u * self.u + self.v * self.v)
         theta = atan2(coef * self.u, coef * self.v) * deg2rad
         return mod, theta
+
+    def get_uv(self):
+        """
+        From module, bearing of a current return u,v velocity components
+
+        :param mod: module of a current
+        :param theta: angle of a current
+        :param wind: for current or wind(True)
+        :return: u,v components of the current
+        """
+        if self.wind:
+            coef = -1
+        else:
+            coef = 1
+
+        rad2deg = pi / 180.
+        u = coef * self. mod * sin(self.theta * rad2deg)
+        v = coef * self.mod * cos(self.theta * rad2deg)
+        return u, v
+
